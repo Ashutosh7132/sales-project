2784d79436d5cb8a296cd6e1a3fb55e321c41507
ashutosh7131 <ashutosh713@outlook.com>
2023-12-16 04:26:28 +0800

    sales person new project

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Ashutosh","Shriansh", "ashutosh.shriansh@maybank.com", 50000),
+    new SalesPerson("Atharv","Doe", "john.doe@luv2code.com", 40000),
+    new SalesPerson("Anvesh","Murphy", "claire.murphy@luv2code.com", 90000),
+    new SalesPerson("Swati","Truong", "mai.truong@luv2code.com", 60000),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
